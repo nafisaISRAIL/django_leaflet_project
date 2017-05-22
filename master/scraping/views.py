0915ad6af30ddfe3f09fb94de64705d02ae988a7
Nafisa <nafisa_ordo@mail.ru>
2017-05-22 21:29:25 +0600

    added markers in main map

--- master/scraping/views.py
+++ master/scraping/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from master.scraping.models import Article
+from datetime import datetime, timedelta
+from master.scraping.models import Article, Category
+from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder
+
+import json
 
 # Create your views here.
 
@@ -10,13 +15,25 @@ def posts(request):
 
 
 def map_view(request):
-    footer = Article.objects.filter(category__name='pedestrian')
-    crash = Article.objects.filter(category__name='crash')
-    bump = Article.objects.filter(category__name='bump')
-    context = {
-        'footer': footer,
-        'crash': crash,
-        'bump': bump
-    }
-    return render(request, 'scraping/map.html', context)
+    filter_cat = request.GET.getlist('category', [])
+    categories = Category.objects.all()
+    delta = datetime.now() - timedelta(days=30)
+    articles = Article.objects.values_list(
+        'id',
+        'latitude',
+        'longitude',
+        'title',
+        'url',
+        'category').filter(
+        created_at__gte=delta,
+        latitude__isnull=False,
+        longitude__isnull=False)
+    if len(filter_cat):
+        articles = articles.filter(category__id__in=filter_cat)
+    filter_cat = [int(x) for x in filter_cat]
 
+    articles = json.dumps(list(articles), cls=DjangoJSONEncoder)
+    return render(request, 'scraping/map.html', {'data': articles,
+                                                 'categories': categories,
+                                                 'filter_cat': filter_cat,
+                                                 })
